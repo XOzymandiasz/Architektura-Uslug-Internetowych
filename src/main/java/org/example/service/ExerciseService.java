80f920d645e63d719506627a57df5b64efd7a0dd
Adam <s198199@student.pg.edu.pl>
2025-10-21 15:53:53 +0200

    feat: add save and delete methods

--- src/main/java/org/example/service/ExerciseService.java
+++ src/main/java/org/example/service/ExerciseService.java
@@ -5,6 +5,7 @@ import org.example.repository.ExerciseRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ExerciseService {
@@ -19,7 +20,17 @@ public class ExerciseService {
     }
 
     public Exercise getByName(String name) {
-        return (Exercise) repository.findByName(name);
+        return repository.findByName(name).getFirst();
     }
 
+    public void save(Exercise exercise) {
+        if (exercise.getId() == null) {
+            exercise.setId(UUID.randomUUID());
+        }
+        repository.save(exercise);
+    }
+
+    public void deleteById(UUID id) {
+        repository.deleteById(id);
+    }
 }
