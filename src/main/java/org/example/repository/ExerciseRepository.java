b11e7226d37a55155cf530b03ac07754db1812d8
Adam <s198199@student.pg.edu.pl>
2025-10-21 15:50:16 +0200

    refactor: changed id type

--- src/main/java/org/example/repository/ExerciseRepository.java
+++ src/main/java/org/example/repository/ExerciseRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface ExerciseRepository extends JpaRepository<Exercise, Integer> {
+public interface ExerciseRepository extends JpaRepository<Exercise, UUID> {
     public List<Exercise> findByName(String name);
 }
--- src/main/java/org/example/repository/ExerciseResultsRepository.java
+++ src/main/java/org/example/repository/ExerciseResultsRepository.java
@@ -2,6 +2,14 @@ package org.example.repository;
 
 import org.example.model.ExerciseResults;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ExerciseResultsRepository extends JpaRepository<ExerciseResults, Integer> {
+import java.util.UUID;
+
+public interface ExerciseResultsRepository extends JpaRepository<ExerciseResults, UUID> {
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("delete from ExerciseResults r where r.id = :id")
+    void hardDeleteById(@Param("id") UUID id);
 }
