b87c7b9300872ad573190e2d2b80492fa6c48f4d
Adam <s198199@student.pg.edu.pl>
2025-10-21 15:53:41 +0200

    feat: add save and delete methods

--- src/main/java/org/example/service/ExerciseResultsService.java
+++ src/main/java/org/example/service/ExerciseResultsService.java
@@ -3,8 +3,10 @@ package org.example.service;
 import org.example.model.ExerciseResults;
 import org.example.repository.ExerciseResultsRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ExerciseResultsService {
@@ -15,5 +17,18 @@ public class ExerciseResultsService {
 
     public List<ExerciseResults> getALl() {
         return repository.findAll();
+    }
+
+    public void save(ExerciseResults results) {
+        if (results.getId() == null) {
+            results.setId(UUID.randomUUID());
+        }
+        repository.save(results);
+    }
+
+    @Transactional
+    public void deleteById(UUID id) {
+        repository.hardDeleteById(id);
+        repository.flush();
     }
 }
