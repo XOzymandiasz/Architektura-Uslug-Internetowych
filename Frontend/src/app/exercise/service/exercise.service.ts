31207426e54b936b631944287f791669e3f9fc38
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2025-12-06 17:39:07 +0100

    feat: create services

---
+++ Frontend/src/app/exercise/service/exercise.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Exercise } from '../model/exercise';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExerciseService {
+  private readonly baseUrl: string = 'api/exercise';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Exercise[]> {
+    return this.http.get<Exercise[]>(
+      `${this.baseUrl}`
+    );
+  }
+
+  getOne(id: string): Observable<Exercise> {
+    return this.http.get<Exercise>(
+      `${this.baseUrl}/${id}`
+    );
+  }
+
+  create(exercise: Exercise): Observable<any> {
+    return this.http.post<Exercise>(
+      `${this.baseUrl}`, exercise
+    );
+  }
+
+  update(exercise: Exercise): Observable<any> {
+    return this.http.put<Exercise>(
+      `${this.baseUrl}`, exercise
+    );
+  }
+
+  delete(exercise: Exercise): Observable<any> {
+    return this.http.delete<Exercise>(
+      `${this.baseUrl}/${exercise.id}`
+    );
+  }
+
+}
---
+++ Frontend/src/app/exercise_result/service/exerciseResult.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ExerciseResult } from '../model/exerciseResult';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Service {
+  private readonly baseUrl = '/api/exercise';
+  private readonly resultUrl = 'result';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(categoryId: string): Observable<ExerciseResult[]> {
+    return this.http.get<ExerciseResult[]>(
+      `${this.baseUrl}/${categoryId}/${this.resultUrl}`
+    );
+  }
+
+  getOne(categoryId:string, id:string): Observable<ExerciseResult> {
+    return this.http.get<ExerciseResult>(
+      `${this.baseUrl}/${categoryId}/${this.resultUrl}/${id}`
+    );
+  }
+
+  create(categoryId:number, result:ExerciseResult) {
+    return this.http.post<ExerciseResult>(
+      `${this.baseUrl}/${categoryId}/${this.resultUrl}`,
+      result
+    );
+  }
+
+  update(categoryId:number, result:ExerciseResult) {
+    return this.http.put<ExerciseResult>(
+      `${this.baseUrl}/${categoryId}/${this.resultUrl}/${result.id}`,
+      result
+    );
+  }
+
+  delete(categoryId: string, id: string): Observable<any> {
+    return this.http.delete<ExerciseResult>(
+      `${this.baseUrl}/${categoryId}/${this.resultUrl}/${id}`
+    );
+  }
+
+}
