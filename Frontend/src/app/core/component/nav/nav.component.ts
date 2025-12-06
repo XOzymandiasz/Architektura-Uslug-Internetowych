6f6d0346c40c967905f93334643905d9832795bc
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2025-12-07 00:18:20 +0100

    feat: add core components

---
+++ Frontend/src/app/core/component/nav/nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-nav',
+  template: `./nav.component.html`,
+  styleUrls: ['./nav.component.css'],
+  standalone: false
+})
+export class NavComponent {
+
+}
