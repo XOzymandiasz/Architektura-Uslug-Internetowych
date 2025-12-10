e0ce3484d0d15a8021e24ca4aa23e26b2a050a37
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2025-12-10 20:15:49 +0100

    feat: add results for Exercise model

--- Frontend/src/app/exercise/model/exercise.ts
+++ Frontend/src/app/exercise/model/exercise.ts
@@ -1,7 +1,10 @@
+import {ExerciseResult} from '../../exercise_result/model/exercise-result';
+
 export interface Exercise {
   id: string;
   name: string;
   muscleGroup: string;
   equipment: string;
   duration: number;
+  results: ExerciseResult[];
 }
