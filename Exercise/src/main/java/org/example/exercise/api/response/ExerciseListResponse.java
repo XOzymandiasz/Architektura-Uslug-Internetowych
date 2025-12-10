954aa4151db852254a493395c0c77bef47bff771
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2025-12-10 20:10:27 +0100

    feat: add more fields for list response

--- Exercise/src/main/java/org/example/exercise/api/response/ExerciseListResponse.java
+++ Exercise/src/main/java/org/example/exercise/api/response/ExerciseListResponse.java
@@ -2,5 +2,11 @@ package org.example.exercise.api.response;
 
 import java.util.UUID;
 
-public record ExerciseListResponse(UUID id, String name) {
+public record ExerciseListResponse(
+        UUID id,
+        String name,
+        String muscleGroup,
+        String equipment,
+        Integer duration
+) {
 }
