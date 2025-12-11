ce6908a881c51c0fa7a1927e343e73a561eca431
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2025-12-11 18:17:40 +0100

    feat: add data initializers

--- Exercise/src/main/java/org/example/exercise/infrastructure/bootstrap/DataInitializer.java
+++ Exercise/src/main/java/org/example/exercise/infrastructure/bootstrap/DataInitializer.java
@@ -1,32 +1,28 @@
-//package org.example.infrastructure.bootstrap;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.example.web.request.ExerciseCreateRequest;
-//import org.example.application.service.ExerciseService;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.UUID;
-//
-//@Component
-//@Order(1)
-//@Profile("dev")
-//@RequiredArgsConstructor
-//public class DataInitializer implements CommandLineRunner {
-//    private final ExerciseService exerciseService;
-//
-//    @Override
-//    public void run(String... args) {
-//            UUID benchId = UUID.randomUUID();
-//            exerciseService.create(new ExerciseCreateRequest(
-//                    benchId, "Bench Press", "Chest", "Barbell", 40
-//            ));
-//
-//            UUID jmId = UUID.randomUUID();
-//            exerciseService.create(new ExerciseCreateRequest(
-//                    jmId, "JM Press", "Chest", "Barbell", 20
-//            ));
-//        }
-//}
+package org.example.exercise.infrastructure.bootstrap;
+
+import lombok.RequiredArgsConstructor;
+import org.example.exercise.api.request.ExerciseCreateRequest;
+import org.example.exercise.application.service.ExerciseService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+    private final ExerciseService exerciseService;
+
+    @Override
+    public void run(String... args) {
+            UUID benchId = UUID.randomUUID();
+            exerciseService.create(new ExerciseCreateRequest(
+                    benchId, "Bench Press", "Chest", "Barbell", 40
+            ));
+
+            UUID jmId = UUID.randomUUID();
+            exerciseService.create(new ExerciseCreateRequest(
+                    jmId, "JM Press", "Chest", "Barbell", 20
+            ));
+        }
+}
--- ExerciseResult/src/main/java/org/example/exercise_result/infrastructure/bootstrap/DataInitializer.java
+++ ExerciseResult/src/main/java/org/example/exercise_result/infrastructure/bootstrap/DataInitializer.java
@@ -1,31 +1,30 @@
-//package org.example.infrastructure.bootstrap;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.example.dto.exercise.result.request.ExerciseResultCreateRequest;
-//import org.example.service.ExerciseResultsService;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.UUID;
-//
-//@Component
-//@Order(1)
-//@Profile("dev")
-//@RequiredArgsConstructor
-//public class DataInitializer implements CommandLineRunner {
-//    private final ExerciseResultsService resultService;
-//
-//    @Override
-//    public void run(String... args) {
-//        resultService.create(benchId, new ExerciseResultCreateRequest(UUID.randomUUID(), 1, 6, 90, false));
-//        resultService.create(benchId, new ExerciseResultCreateRequest(UUID.randomUUID(), 2, 6, 90, false));
-//        resultService.create(benchId, new ExerciseResultCreateRequest(UUID.randomUUID(), 3, 5, 90, false));
-//
-//        resultService.create(jmId, new ExerciseResultCreateRequest(UUID.randomUUID(), 1, 8, 70, true));
-//        resultService.create(jmId, new ExerciseResultCreateRequest(UUID.randomUUID(), 2, 8, 75, true));
-//        resultService.create(jmId, new ExerciseResultCreateRequest(UUID.randomUUID(), 3, 5, 80, false));
-//        resultService.create(jmId, new ExerciseResultCreateRequest(UUID.randomUUID(), 4, 3, 80, false));
-//    }
-//}
+package org.example.exercise_result.infrastructure.bootstrap;
+
+import lombok.RequiredArgsConstructor;
+import org.example.exercise_result.api.request.ExerciseResultCreateRequest;
+import org.example.exercise_result.application.service.ExerciseResultsService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+    private final ExerciseResultsService resultService;
+    private final UUID benchId = UUID.fromString("630feb5a-37d2-47e1-9e75-0cae5be13180");
+    private final UUID JMId = UUID.fromString("4dca6bc6-6001-4408-9d04-50a640a8ebff");
+
+
+    @Override
+    public void run(String... args) {
+        resultService.create(benchId, new ExerciseResultCreateRequest(UUID.randomUUID(), 1, 6, 90, false));
+        resultService.create(benchId, new ExerciseResultCreateRequest(UUID.randomUUID(), 2, 6, 90, false));
+        resultService.create(benchId, new ExerciseResultCreateRequest(UUID.randomUUID(), 3, 5, 90, false));
+
+        resultService.create(JMId, new ExerciseResultCreateRequest(UUID.randomUUID(), 1, 8, 70, true));
+        resultService.create(JMId, new ExerciseResultCreateRequest(UUID.randomUUID(), 2, 8, 75, true));
+        resultService.create(JMId, new ExerciseResultCreateRequest(UUID.randomUUID(), 3, 5, 80, false));
+        resultService.create(JMId, new ExerciseResultCreateRequest(UUID.randomUUID(), 4, 3, 80, false));
+    }
+}
