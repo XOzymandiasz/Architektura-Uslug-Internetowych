67eb0cafc63dc25181d1247cb51361bb446a1a96
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-01-09 14:12:53 +0100

    feat: config eureka

--- Eureka/src/main/java/org/example/Main.java
+++ Eureka/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class Main {
+
+    /**
+     * Application main entry point.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(Main.class, args);
     }
+
 }
